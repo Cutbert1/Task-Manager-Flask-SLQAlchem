f879c891caf95b588e92164c8aafc8f27d23c9bb
Cutbert Ogbozor <cutbertogbozor@yahoo.co.uk>
2025-01-22 16:04:06 +0000

    Create add_task.html from add_category.html and updated code. Add app route for add_task and updated script.js with datepicker and select initailization

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,4 +2,16 @@ document.addEventListener("DOMContentLoaded", function() {
     // sidenav initialization
     let sidenav = document.querySelectorAll(".sidenav");
     M.Sidenav.init(sidenav);
+
+    //datepicker Intialization
+    let datepicker = document.querySelectorAll(".datepicker");
+    M.Datepicker.init(datepicker, {
+        format: "dd mmmm, yyyy",
+        i18n: {done: "Select"}
+    });
+
+    // select Initialization
+    let selects = document.querySelectorAll("select");
+    M.FormSelect.init(selects)
 });
+
